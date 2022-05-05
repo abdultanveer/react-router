ef7c3ef923ccf6749b7ed9f8453ec2392d10e8e1
abdul <abdul.tanveer@gmail.com>
2022-05-05 15:37:52 +0530

    navigating back

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export const OrderSummary = () => {
+    const navigate = useNavigate()
+
   return (
+      <>
     <div>Order confirmed -- on the way</div>
+    <button onClick={() => navigate(-1)}>back</button>
+
+    </>
   )
 }
