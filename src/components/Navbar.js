61a824cc675da70e002d1f039e671853ade0722d
abdul <abdul.tanveer@gmail.com>
2022-05-05 15:18:21 +0530

    nav link style js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,20 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const navLinkStyles =({isActive}) =>{
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+
+
+
   return (
     <nav>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/about'>About</NavLink>
+        <NavLink style = {navLinkStyles} to='/'>Home</NavLink>
+        <NavLink style={navLinkStyles} to='/about'>About</NavLink>
 
     </nav>
   )
